244b4f4730bdb89fcf23baa74768f9d6fb8c7a5d
oscartena <oscar.tena.marin@gmail.com>
2023-10-27 19:38:44 +0200

    modificacion practica2

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button newPlayer;
@@ -23,10 +26,23 @@ public class MainActivity extends AppCompatActivity {
                 intent.setClass(MainActivity.this,NewPlayer.class);
                 startActivity(intent);
             }
+
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id= item.getItemId();
+        if (id==R.id.action_buscar){
+            Toast.makeText(this,"Busqueda",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/practica1/NewPlayer.java
+++ app/src/main/java/com/example/practica1/NewPlayer.java
@@ -3,6 +3,9 @@ package com.example.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NewPlayer extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class NewPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newplayer);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id= item.getItemId();
+        if (id==R.id.action_buscar){
+            Toast.makeText(this,"Busqueda",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
